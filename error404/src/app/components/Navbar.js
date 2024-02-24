2657fa801fc2f80648b1f5ef15cc76d52ef8d5a2
RyanSikandar <ryansikandar@gmail.com>
2024-02-25 02:57:45 +0500

    navbar final work done

--- error404/src/app/components/Navbar.js
+++ error404/src/app/components/Navbar.js
@@ -1,9 +1,35 @@
+"use client"
 import Image from "next/image"
 import PakTravels from "../../../public/PakTravels.webp"
 import Link from "next/link"
+import { useEffect, useState } from "react";
 export default function Navbar() {
+    const [isHeaderShown, setIsHeaderShown] = useState(false);
+    let lastScrollTop = 0;
+
+    useEffect(() => {
+
+        const handleScroll = () => {
+            const st = window.scrollY;
+            if (st < 10) {
+                setIsHeaderShown(false);
+            } else if (st > lastScrollTop) {
+                setIsHeaderShown(true);
+            } else {
+                setIsHeaderShown(true);
+            }
+        };
+        window.addEventListener("scroll", handleScroll);
+        let width = window.innerWidth;
+        if (width < 768) {
+            setIsMobile(true);
+        }
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
     return (
-        <div className="justify-between flex fixed top-0 z-10 bg-transparent w-screen px-4">
+        <div className={`justify-between flex fixed top-0 z-10 bg-${isHeaderShown ? "white" : "transparent"} w-screen px-4 transition-all ease-in-out duration-200`}>
             <Image src={PakTravels} alt="Logo" height={170} width={170} />
 
             <div>
