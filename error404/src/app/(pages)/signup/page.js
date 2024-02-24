3d4944d417530ca4949f575be463d6003a843611
waqas2714 <waqasali00123@gmail.com>
2024-02-24 18:51:53 +0500

    signup page

---
+++ error404/src/app/(pages)/signup/page.js
@@ -0,0 +1,130 @@
+import Navbar from "@/app/components/Navbar";
+
+const signup = () => {
+  const travelTypes = [
+    "Adventure",
+    "Family",
+    "Agritourism",
+    "Luxury",
+    "Business",
+  ];
+  return (
+    <div>
+      <Navbar />
+      <div
+        className="min-h-screen max-h-fit mt-[30vh] flex justify-center items-center"
+        style={{ background: "url(/BM-503.webp) center/cover no-repeat" }}
+      >
+        <label className="form-control max-w-x bg-[#EDBE73] opacity-100 p-10 rounded-lg">
+          <p className="text-center text-2xl font-extrabold">Signup</p>
+          <div className="label">
+            <span className="label-text font-bold text-lg text-black">
+              Name
+            </span>
+          </div>
+          <input
+            type="text"
+            placeholder="Name"
+            className="input input-bordered w-full max-w-xs h-10"
+          />
+
+          <div className="label">
+            <span className="label-text font-bold text-lg  text-black">
+              Ph No.
+            </span>
+          </div>
+          <input
+            type="text"
+            placeholder="03XX-XXXXXXX"
+            className="input input-bordered w-full max-w-xs h-10"
+          />
+
+          <div className="label">
+            <span className="label-text font-bold text-lg  text-black">
+              Email
+            </span>
+          </div>
+          <input
+            type="email"
+            placeholder="Email"
+            className="input input-bordered w-full max-w-xs h-10"
+          />
+
+          <div className="label">
+            <span className="label-text font-bold text-lg  text-black">
+              Password
+            </span>
+          </div>
+
+          <input
+            type="password"
+            placeholder="Password"
+            className="input input-bordered w-full max-w-xs h-10"
+          />
+
+          <div className="label">
+            <span className="label-text font-bold text-lg  text-black">
+              Confirm Password
+            </span>
+          </div>
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            className="input input-bordered w-full max-w-xs h-10"
+          />
+
+          <div className="label">
+            <span className="label-text font-bold text-lg  text-black">
+              Budget
+            </span>
+          </div>
+          <input
+            type="text"
+            placeholder="Rs."
+            className="input input-bordered w-full max-w-xs h-10"
+          />
+
+          <div className="label">
+            <span className="label-text font-bold text-lg  text-black">
+              Travel Type
+            </span>
+          </div>
+          <details className="dropdown">
+            <summary className="m-1 btn">Adventure</summary>
+            <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+              {travelTypes.map((item, index) => {
+                return (
+                  <li key={index}>
+                    <a>{item}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </details>
+
+          <div className="label">
+            <span className="label-text font-bold text-lg  text-black">
+              Preferable Distance
+            </span>
+          </div>
+
+          <input
+            type="text"
+            placeholder="Km"
+            className="input input-bordered w-full max-w-xs h-10"
+          />
+
+          <div className="label">
+            <span className="label-text font-bold text-lg  text-black">
+              Current Location
+            </span>
+          </div>
+
+          <button className="btn mt-5 h-1 opacity-100">Signup</button>
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default signup;
