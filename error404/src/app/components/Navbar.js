8445ae2681b3f455a133f6f1e16676fb05e10793
RyanSikandar <ryansikandar@gmail.com>
2024-02-24 15:51:26 +0500

    set up daisy ui

---
+++ error404/src/app/components/Navbar.js
@@ -0,0 +1,15 @@
+import PakTravels from "error404\public\PakTravels.webp"
+export default function Navbar() {
+  return (
+    <div className="justify-around flex">
+    <button class="btn w-64 rounded-full">Button</button>
+    <div>
+    
+    </div>
+    <div>
+    lamooo
+    </div>
+
+    </div>
+  )
+}
