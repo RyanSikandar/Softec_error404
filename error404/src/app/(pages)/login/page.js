8600cd480af47bb852b2b5c4a3282e465dba49f3
waqas2714 <waqasali00123@gmail.com>
2024-02-24 18:51:41 +0500

    login page

--- error404/src/app/(pages)/login/page.js
+++ error404/src/app/(pages)/login/page.js
@@ -4,8 +4,8 @@ const login = () => {
   return (
     <div>
       <Navbar />
-      <div className="h-[75vh] flex justify-center items-center" style={{backgroundImage : "url(/badshahi-mosque.webp)"}}>
-        <label className="form-control max-w-x bg-[#EDBE73] opacity-80 p-10 rounded-lg">
+      <div className="h-screen flex justify-center items-center" style={{background : "url(/BM-503.webp) center/cover no-repeat"}}>
+        <label className="form-control max-w-x bg-[#EDBE73] opacity-100 p-10 rounded-lg">
           <p className="text-center text-2xl font-extrabold">Login</p>
           <div className="label">
             <span className="label-text font-bold text-lg text-black">Email</span>
@@ -25,7 +25,7 @@ const login = () => {
             className="input input-bordered w-full max-w-xs h-10"
           />
 
-          <button className="btn mt-5 h-1">Login</button>
+          <button className="btn mt-5 h-1 opacity-100">Login</button>
         </label>
       </div>
     </div>
