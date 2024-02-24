26aeb13fceaf261ef105058907002ece5e759b02
RyanSikandar <ryansikandar@gmail.com>
2024-02-24 16:24:07 +0500

    wattay navy

--- error404/src/app/components/Navbar.js
+++ error404/src/app/components/Navbar.js
@@ -1,15 +1,16 @@
-import PakTravels from "error404\public\PakTravels.webp"
+import Image from "next/image"
+import PakTravels from "../../../public/PakTravels.webp"
 export default function Navbar() {
-  return (
-    <div className="justify-around flex">
-    <button class="btn w-64 rounded-full">Button</button>
-    <div>
-    
-    </div>
-    <div>
-    lamooo
-    </div>
+    return (
+        <div className="justify-between flex">
+          <Image src={PakTravels} alt="Logo" height={170} width={170}/>
+            <div>
+            
+            </div>
+            <div>
+                lamooo
+            </div>
 
-    </div>
-  )
+        </div>
+    )
 }
