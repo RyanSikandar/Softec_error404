f2a680abeabb493f0db0afd10793f40853ebbe93
RyanSikandar <ryansikandar@gmail.com>
2024-02-24 16:52:16 +0500

    navbar complete

--- error404/src/app/components/Navbar.js
+++ error404/src/app/components/Navbar.js
@@ -1,14 +1,17 @@
 import Image from "next/image"
 import PakTravels from "../../../public/PakTravels.webp"
+import Link from "next/link"
 export default function Navbar() {
     return (
         <div className="justify-between flex">
           <Image src={PakTravels} alt="Logo" height={170} width={170}/>
+        
             <div>
             
             </div>
-            <div>
-                lamooo
+            <div className="flex justify-between mt-12 gap-20 mr-4">
+            <button className="btn btn-outline bg-[#EDBE73] text-black hover:bg-[#60422A] ">Default</button>
+            <button className="btn btn-outline btn-accent">Plan a trip</button>
             </div>
 
         </div>
