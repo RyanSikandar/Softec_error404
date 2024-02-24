aa8080acae862eac09228a6ec2bf713090ce22a1
RyanSikandar <ryansikandar@gmail.com>
2024-02-24 17:28:46 +0500

    video added in background

--- error404/src/app/page.js
+++ error404/src/app/page.js
@@ -4,6 +4,16 @@ export default function Home() {
   return (
     <main>
       <Navbar/>
+      <div className="relative h-[calc(95vh)]">
+        <video
+          autoPlay
+          muted
+          loop
+          style={{ width: "100%", height: "100vh", objectFit: "cover" }}
+        >
+          <source src="/touristvideo.webm" />
+        </video>
+      </div>
     </main>
   );
 }
